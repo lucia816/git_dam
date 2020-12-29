eaac6a74e74a7709ce7671b612d02fcdb6de995b
HP <HP@DESKTOP-R38POL5.home>
2020-12-29 14:41:03 +0100

    hola

--- entregas/src/entregasclase/entreganav.java
+++ entregas/src/entregasclase/entreganav.java
@@ -7,12 +7,18 @@ public class entreganav {
 		// TODO Auto-generated method stub
 		String fichero = "C:\\Users\\HP\\git\\repository\\entregas\\src\\entregasclase\\tareasacompletar";
 		try {
-			PrintWriter pw = new PrintWriter(new File (fichero));
+			FileWriter pw = new FileWriter(new File (fichero),true);
 			Scanner sc = new Scanner (System.in);
 			System.out.println("Introduce la tarea a realizar ");
-			String cumpleaños = sc.next();
-			pw.print(cumpleaños);
+			String incomptarea = sc.next();
+			pw.write(incomptarea);
+			sc.nextLine();
 			pw.close();
+			
+			while (sc.hasNextLine()) {
+				String linea = sc.nextLine(); 
+				System.out.println(linea);  
+			}
 		} catch (Exception e) {
 			System.err.println("Problema para abrir el archivo");
 		}
