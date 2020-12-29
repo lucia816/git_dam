2d1e06917dfcc4b947b3e61cf801a10bd9c5c76e
HP <HP@DESKTOP-R38POL5.home>
2020-12-29 15:39:08 +0100

    hola

---
+++ entregas/src/entregasclase/Hastable.java
@@ -0,0 +1,13 @@
+package entregasclase;
+import java.util.*;
+public class Hastable {
+	Hashtable contenedor = new Hashtable();
+	
+	public Hastable() {
+		// TODO Auto-generated constructor stub
+		contenedor.put("hola", true);
+		
+		System.out.println(contenedor);
+	}
+
+}
