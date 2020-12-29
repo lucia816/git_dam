8bb85f698c024d1a0b12d248b89b18c6a85d6835
HP <HP@DESKTOP-R38POL5.home>
2020-12-29 17:35:40 +0100

    hola

--- entregas/src/entregasclase/entreganav.java
+++ entregas/src/entregasclase/entreganav.java
@@ -1,22 +1,29 @@
 package entregasclase;
 import java.io.*;
+import java.util.Date;
 import java.util.*;
 public class entreganav {
 
 	public static void main(String[] args) {
-		    
+		   Date fecha = new Date();  
 		try {
 			FileWriter fichero =  new FileWriter ("C:\\Users\\HP\\git\\repository\\entregas\\src\\entregasclase\\tareasacompletar",true);
 			Hashtable tareas  = new Hashtable();
+			System.out.println("Tareas pendientes: ");
 			tareas.put("Poner arbol",true);
-			tareas.put("Matematica",true);
+			tareas.put("Poner la mesa",true);
 			
 			
-			Integer minumero = (Integer) tareas.get("numero");
 
-			System.out.println("Poner arbol: " + tareas.get("Poner arbol"));
+			System.out.println("1- Poner arbol: " + tareas.get("Poner arbol"));
+			System.out.println("2- Poner la mesa: " + tareas.get("Poner la mesa"));
 			PrintWriter pw = new PrintWriter(fichero);
-			pw.println("Poner arbol: " +tareas.get("Poner arbol"));
+			pw.println("Tareas pendientes" );
+			pw.println("-------------------");
+			pw.println("1- Poner arbol: " +tareas.get("Poner arbol"));
+			pw.println(fecha);
+			pw.println("2- Poner la mesa: " +tareas.get("Poner la mesa"));
+			pw.println(fecha);
 			pw.close();
 			
 		
