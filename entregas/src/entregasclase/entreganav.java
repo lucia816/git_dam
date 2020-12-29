c1d863742f557c770f49fc27598bdc1ce7b69341
HP <HP@DESKTOP-R38POL5.home>
2020-12-29 15:12:59 +0100

    hola

--- entregas/src/entregasclase/entreganav.java
+++ entregas/src/entregasclase/entreganav.java
@@ -4,26 +4,27 @@ import java.util.Scanner;
 public class entreganav {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		char [] letra= {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'}; 
-		String tareai;
+		
+		String tareainc;
 		
 		Scanner sc = new Scanner (System.in);
 		System.out.println("Introduzca la terea a realizar ");
-		tareai = sc.next();
+		tareainc = sc.next();
+		
 		try {
 			FileWriter fichero =  new FileWriter ("C:\\Users\\HP\\git\\repository\\entregas\\src\\entregasclase\\tareasacompletar",true);
 			
 			PrintWriter pw = new PrintWriter(fichero);
-			
-			pw.println(tareai);
-			
+			pw.println(tareainc);
 			pw.close();
-			
+		paises.put 
 	
 		} catch (Exception e) {
 			System.err.println("Problema para abrir el archivo");
 		}
 	}
+	public class Hashtable {
+	Hashtable paises = new Hashtable();
 
+	}
 }
