acaecfe5f23d4610619f26f13a3edbbfb2a302d7
HP <HP@DESKTOP-R38POL5.home>
2020-12-31 17:08:54 +0100

    buenas

--- entregas/src/entregasclase/entreganav.java
+++ entregas/src/entregasclase/entreganav.java
@@ -17,18 +17,31 @@ public class entreganav {
 			Hashtable tareascomp = new Hashtable (); 
 			
 			System.out.println("Ingresar las tareas pendientes (hasta 5)");
+			
 			tarea1 = sc.next();
 			tarea2 = sc.next();
 			tarea3 = sc.next();
 			tarea4 = sc.next();
 			tarea5 = sc.next();
 			
+			tareas.put(tarea1, true);
+			tareas.put(tarea2, true);
+			tareas.put(tarea3, true);
+			tareas.put(tarea4, true);
+			tareas.put(tarea5, true);
+			
 			PrintWriter pw = new PrintWriter(fichero);
+			
 			pw.println(tarea1);
+			pw.println(fecha);
 			pw.println(tarea2);
+			pw.println(fecha);
 			pw.println(tarea3);
+			pw.println(fecha);
 			pw.println(tarea4);
+			pw.println(fecha);
 			pw.println(tarea5);
+			pw.println(fecha);
 			
 			System.out.println("Que desea hacer? ");
 			System.out.println("1- Crear una nueva tarea pendiente ");
@@ -52,7 +65,8 @@ public class entreganav {
 				}
 			case 2: 
 				if (opcionqhacer == 2) {
-					
+					pw.println();
+					pw.println("Tareas pendientes");
 					pw.println(tareas);
 					pw.println(fecha);
 					break;
@@ -60,17 +74,22 @@ public class entreganav {
 			case 3: 
 				
 				if (opcionqhacer== 3) {
+					
 					System.out.println("Cual quieres modificar? ");
-					System.out.println("1- Poner arbol");
-					System.out.println("2- Poner la mesa ");
-					System.out.println("3- El ingresado anteriormente");
+					System.out.println("1- tarea 1");
+					System.out.println("2- Tarea 2");
+					System.out.println("3- Tarea 3");
+					System.out.println("4- Tarea 4");
+					System.out.println("5- Tarea 5");
+					
+					
 					opcionqeliminar = sc.nextInt();
 					switch (opcionqeliminar) {
 					case 1:
 						if (opcionqeliminar == 1) {
 							
-								tareas.remove("Poner arbol");
-								tareascomp.put("Poner arbol", true);
+								tareas.remove(tarea1);
+								tareascomp.put(tarea1, true);
 								
 								pw.println("Tareas aun pendientes ");
 								pw.println("----------------------");
@@ -84,34 +103,68 @@ public class entreganav {
 					case 2:
 						if (opcionqeliminar == 2) {
 							
-							tareas.remove("Poner la mesa ");
-							
+							tareas.remove(tarea2);
+							tareascomp.put(tarea2, true);
 							
 							pw.println("Tareas aun pendientes");
 							pw.println("-----------------------");
 							pw.println(tareas);
 							pw.println();
-							tareascomp.put("Poner la mesa ", true);
+							
 							pw.println("Tareas completas ");
 							pw.println("------------------");
 							pw.println(tareascomp);
 							break;
 						}
 					case 3:
-						if (opcionqeliminar == 3){
-							tareas.remove(sc);
-						
+						if(opcionqeliminar == 3) {
+							tareas.remove(tarea3);
+							tareascomp.put(tarea3, true);
+							
 							pw.println("Tareas aun pendientes");
 							pw.println("-----------------------");
 							pw.println(tareas);
 							pw.println();
-							tareascomp.put("Poner la mesa ", true);
+							
 							pw.println("Tareas completas ");
 							pw.println("------------------");
 							pw.println(tareascomp);
 							break;
+						
+						}
+					case 4:
+						if (opcionqeliminar == 4) {
+							tareas.remove(tarea4);
+							tareascomp.put(tarea4, true);
+							
+							pw.println("Tareas aun pendientes");
+							pw.println("-----------------------");
+							pw.println(tareas);
+							pw.println();
+							
+							pw.println("Tareas completas ");
+							pw.println("------------------");
+							pw.println(tareascomp);
+							break;
+							
+						}
+					case 5:
+						if (opcionqeliminar == 5) {
+							tareas.remove(tarea5);
+						tareascomp.put(tarea5, true);
+						
+						pw.println("Tareas aun pendientes");
+						pw.println("-----------------------");
+						pw.println(tareas);
+						pw.println();
+						
+						pw.println("Tareas completas ");
+						pw.println("------------------");
+						pw.println(tareascomp);
+						break;
 						}
 					}
+					
 				}
 			}
 			pw.close();
