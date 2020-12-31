ca044925cb8e510dd42cfda5b020c00a57e2d8a7
HP <HP@DESKTOP-R38POL5.home>
2020-12-31 16:43:44 +0100

    final?

--- entregas/src/entregasclase/entreganav.java
+++ entregas/src/entregasclase/entreganav.java
@@ -9,18 +9,32 @@ public class entreganav {
 		   int opcionqhacer;
 		   int opcionqeliminar;
 		   int cualelimina;
+		   String tarea1,tarea2,tarea3,tarea4,tarea5;
 		   Scanner sc = new Scanner (System.in);
 		try {
 			FileWriter fichero =  new FileWriter ("C:\\Users\\HP\\git\\repository\\entregas\\src\\entregasclase\\tareasacompletar",true);
 			Hashtable tareas  = new Hashtable();
 			Hashtable tareascomp = new Hashtable (); 
-			tareas.put("Poner arbol", true);
-			tareas.put("Poner la mesa ",true);
+			
+			System.out.println("Ingresar las tareas pendientes (hasta 5)");
+			tarea1 = sc.next();
+			tarea2 = sc.next();
+			tarea3 = sc.next();
+			tarea4 = sc.next();
+			tarea5 = sc.next();
+			
+			PrintWriter pw = new PrintWriter(fichero);
+			pw.println(tarea1);
+			pw.println(tarea2);
+			pw.println(tarea3);
+			pw.println(tarea4);
+			pw.println(tarea5);
 			
 			System.out.println("Que desea hacer? ");
 			System.out.println("1- Crear una nueva tarea pendiente ");
 			System.out.println("2- Mostrar las tareas pendientes ");
 			System.out.println("3- Dar por terminada una tarea pendiente ");
+			
 			opcionqhacer = sc.nextInt();
 			switch (opcionqhacer){
 			
@@ -29,8 +43,8 @@ public class entreganav {
 					System.out.println("Indicar la nueva tarea ");
 					String tareanueva = sc.next();
 					
-					tareas.put(sc, true);
-					PrintWriter pw = new PrintWriter(fichero);
+					tareas.put(tareanueva, true);
+					
 					pw.println(tareanueva);
 					pw.println(fecha);
 					
@@ -38,7 +52,7 @@ public class entreganav {
 				}
 			case 2: 
 				if (opcionqhacer == 2) {
-					PrintWriter pw = new PrintWriter(fichero);
+					
 					pw.println(tareas);
 					pw.println(fecha);
 					break;
@@ -49,15 +63,15 @@ public class entreganav {
 					System.out.println("Cual quieres modificar? ");
 					System.out.println("1- Poner arbol");
 					System.out.println("2- Poner la mesa ");
+					System.out.println("3- El ingresado anteriormente");
 					opcionqeliminar = sc.nextInt();
-					
 					switch (opcionqeliminar) {
 					case 1:
 						if (opcionqeliminar == 1) {
 							
 								tareas.remove("Poner arbol");
 								tareascomp.put("Poner arbol", true);
-								PrintWriter pw = new PrintWriter(fichero);
+								
 								pw.println("Tareas aun pendientes ");
 								pw.println("----------------------");
 								pw.println(tareas);
@@ -72,7 +86,21 @@ public class entreganav {
 							
 							tareas.remove("Poner la mesa ");
 							
-							PrintWriter pw = new PrintWriter(fichero);
+							
+							pw.println("Tareas aun pendientes");
+							pw.println("-----------------------");
+							pw.println(tareas);
+							pw.println();
+							tareascomp.put("Poner la mesa ", true);
+							pw.println("Tareas completas ");
+							pw.println("------------------");
+							pw.println(tareascomp);
+							break;
+						}
+					case 3:
+						if (opcionqeliminar == 3){
+							tareas.remove(sc);
+						
 							pw.println("Tareas aun pendientes");
 							pw.println("-----------------------");
 							pw.println(tareas);
@@ -86,8 +114,6 @@ public class entreganav {
 					}
 				}
 			}
-			
-			PrintWriter pw = new PrintWriter(fichero);
 			pw.close();
 			
 		} catch (Exception e) {
